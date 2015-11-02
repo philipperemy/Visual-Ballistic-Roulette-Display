a1929ef160a574d910a54a5c040357590e989311
Philippe Rémy <philippe.remy@junior-aei.com>
2015-11-02 17:45:23 +0900

    Add responsive voice

--- js/ajax.js
+++ js/ajax.js
@@ -1,6 +1,10 @@
 $( document ).ready(function() {
   setInterval(function(){
-		$("#outcome").load('http://localhost:8080/RouletteServer/Response')
-	}, 2000);
+		$("#outcome").load('http://localhost:8080/RouletteServer/Response');
+		var htmlVal = $("#outcome").html();
+		console.log(htmlVal);
+		//responsiveVoice.speak("32", "French Female");
+		responsiveVoice.speak(htmlVal, "French Female");
+	}, 5000);
 });
 

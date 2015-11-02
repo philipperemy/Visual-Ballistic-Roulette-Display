e390cf6d18ae3a8df3276d3c4bb79b7730cc7371
Philippe Rémy <philippe.remy@junior-aei.com>
2015-11-02 18:01:40 +0900

    Working

--- js/ajax.js
+++ js/ajax.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
   setInterval(function(){
-		$("#outcome").load('http://localhost:8080/RouletteServer/Response');
+		$("#outcome").load('http://localhost:8080/RouletteServer/Response?sessionid=1');
 		var htmlVal = $("#outcome").html();
 		console.log(htmlVal);
 		//responsiveVoice.speak("32", "French Female");
 		responsiveVoice.speak(htmlVal, "French Female");
-	}, 5000);
+	}, 6000);
 });
 

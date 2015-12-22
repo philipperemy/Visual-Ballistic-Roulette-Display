62035ca7ed149ee12be4da15481b8a3e3c614f4c
Philippe Rémy <philippe.remy@junior-aei.com>
2015-12-22 19:45:48 +0900

    working stage

--- js/visual-roulette-api-1.0.js
+++ js/visual-roulette-api-1.0.js
@@ -1,24 +1,33 @@
-﻿
-$(document).ready(function() {
-    
-	setInterval(function(){ // continuously check if the audio is being played
-	  if(responsiveVoice.isPlaying()) {
-			console.log('playing..');
-	}}, 100);
-	
-	setInterval(function() {
-		$.get("http://localhost:8080/RouletteServer/Response", function(data){
-			
-			$("#outcome").html(data);
-			if(data.indexOf("SESSION_NOT_READY_WHEEL_COUNT_ACTUAL") > -1) {
-				var wheelCountActual = data.split(",")[1];
-				responsiveVoice.speak("Not ready. Wheel count at " + wheelCountActual, "UK English Female");
-			} else {
-				responsiveVoice.speak(data, "UK English Female");
-			}
-			
+﻿$(document).ready(function() {
+
+	var spinLock = false; //Unlock
+
+    setInterval(function() { // continuously check if the audio is being played
+        if (responsiveVoice.isPlaying()) {
+            console.log('playing..');
+			spinLock = true; //Lock
+        } else {
+			console.log('no more..');
+			spinLock = false; //Unlock
+			callApi();
+		}
+    }, 100);
+
+	function callApi() {
+        //$.get("http://localhost:8080/RouletteServer/Response", function(data) {
+        $.get("http://localhost:8080/RouletteServer/Response?sessionid=1", function(data) {
+
+            $("#outcome").html(data);
+            if (data.indexOf("SESSION_NOT_READY_WHEEL_COUNT_ACTUAL") > -1) {
+                var wheelCountActual = data.split(",")[1];
+                responsiveVoice.speak("Not ready. Wheel count at " + wheelCountActual, "UK English Female");
+            } else {
+                responsiveVoice.speak(data, "UK English Female");
+            }
+
         }).fail(function() {
-			alert( "error" );
-		  });
-    }, 5000);
+            alert("error");
+        });
+    }
+	
 });

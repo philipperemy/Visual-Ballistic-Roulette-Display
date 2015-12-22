d2019ee19d57bd955cb280703965a46a055f5f35
Philippe Rémy <philippe.remy@junior-aei.com>
2015-12-22 20:22:49 +0900

    format JS

--- js/visual-roulette-api-1.0.js
+++ js/visual-roulette-api-1.0.js
@@ -1,29 +1,29 @@
 ﻿$(document).ready(function() {
 
-	var spinLock = false; //Unlock
-	var scriptResponseTime = 200; // ms
-	
-	// continuously check if the audio is being played
-    var timerID = setInterval(function() { 
+    var spinLock = false; //Unlock
+    var scriptResponseTime = 200; // ms
+
+    // continuously check if the audio is being played
+    var timerID = setInterval(function() {
         if (responsiveVoice.isPlaying()) {
             console.log('playing..');
-			spinLock = true; //Lock
+            spinLock = true; //Lock
         } else {
-			console.log('no more..');
-			spinLock = false; //Unlock
-			callApi();
-		}
+            console.log('no more..');
+            spinLock = false; //Unlock
+            callApi();
+        }
     }, scriptResponseTime);
 
-	function callApi() {
-		
-		var target = "http://localhost:8080/RouletteServer/Response";
+    function callApi() {
+
+        var target = "http://localhost:8080/RouletteServer/Response";
         //TODO: remove it
-		if(Math.random() < 0.1) {
-			target = "http://localhost:8080/RouletteServer/Response?sessionid=1"
-		}
-		
-		$.get(target, function(data) {
+        if (Math.random() < 0.1) {
+            target = "http://localhost:8080/RouletteServer/Response?sessionid=1"
+        }
+
+        $.get(target, function(data) {
 
             $("#outcome").html(data);
             if (data.indexOf("SESSION_NOT_READY_WHEEL_COUNT_ACTUAL") > -1) {
@@ -34,15 +34,15 @@
             }
 
         }).fail(function() {
-			error();
-            
+            error();
+
         });
     }
-	
-	function error() {
-		$("#outcome").html("Critical error.");
-		clearInterval(timerID);
-		responsiveVoice.speak("Critical error.", "UK English Female");
-	}
-	
+
+    function error() {
+        $("#outcome").html("Critical error.");
+        clearInterval(timerID);
+        responsiveVoice.speak("Critical error.", "UK English Female");
+    }
+
 });
